66b7a8943439a0d2519f01f09fc77a00e3fdf796
coyoteatento-debug <coyoteatento@gmail.com>
2026-05-16 14:53:59 -0600

    Update trip_record.dart

--- lib/data/models/trip_record.dart
+++ lib/data/models/trip_record.dart
@@ -13,13 +13,27 @@ class TripRecord {
     this.routeCoords = const [],
   });
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() {
+  final coords = _sampleCoords(routeCoords, maxPoints: 100);
+  return {
     'destination': destination,
-    'distanceKm': distanceKm,
+    'distanceKm':  distanceKm,
     'durationMin': durationMin,
-    'date': date.toIso8601String(),
-    'routeCoords': routeCoords,
+    'date':        date.toIso8601String(),
+    'routeCoords': coords,
   };
+}
+
+List<List<double>> _sampleCoords(
+    List<List<double>> coords, {required int maxPoints}) {
+  if (coords.length <= maxPoints) return coords;
+  final step   = (coords.length - 1) / (maxPoints - 1);
+  final result = <List<double>>[];
+  for (int i = 0; i < maxPoints; i++) {
+    result.add(coords[(i * step).round()]);
+  }
+  return result;
+}
 
   factory TripRecord.fromJson(Map<String, dynamic> j) => TripRecord(
     destination: j['destination'],
