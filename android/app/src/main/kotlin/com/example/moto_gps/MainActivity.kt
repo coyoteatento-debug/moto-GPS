6d99c173cc9925d24aba19262a4aed1c27387d68
coyoteatento-debug <coyoteatento@gmail.com>
2026-05-16 13:32:53 -0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/moto_gps/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moto_gps/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.coyoteatento.motogps
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
@@ -10,6 +15,38 @@ class MainActivity : FlutterActivity() {
 
     private val METHOD_CHANNEL = "com.coyoteatento.motogps/background"
     private val EVENT_CHANNEL  = "com.coyoteatento.motogps/location"
+
+    private var eventSink: EventChannel.EventSink? = null
+    private var localBroadcast: LocalBroadcastManager? = null
+
+    private val locationReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action != LocationForegroundService.ACTION_LOCATION) return
+            val lat     = intent.getDoubleExtra(LocationForegroundService.EXTRA_LAT,     0.0)
+            val lng     = intent.getDoubleExtra(LocationForegroundService.EXTRA_LNG,     0.0)
+            val speed   = intent.getFloatExtra(LocationForegroundService.EXTRA_SPEED,   0f)
+            val bearing = intent.getFloatExtra(LocationForegroundService.EXTRA_BEARING, 0f)
+            runOnUiThread {
+                eventSink?.success(mapOf(
+                    "latitude"  to lat,
+                    "longitude" to lng,
+                    "speed"     to speed,
+                    "heading"   to bearing
+                ))
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        localBroadcast = LocalBroadcastManager.getInstance(this)
+    }
+
+    override fun onDestroy() {
+        localBroadcast?.unregisterReceiver(locationReceiver)
+        eventSink = null
+        super.onDestroy()
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -53,19 +90,15 @@ class MainActivity : FlutterActivity() {
             EVENT_CHANNEL
         ).setStreamHandler(object : EventChannel.StreamHandler {
             override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                LocationForegroundService.onLocationUpdate = { lat, lng, speed, bearing ->
-                    runOnUiThread {
-                        events?.success(mapOf(
-                            "latitude"  to lat,
-                            "longitude" to lng,
-                            "speed"     to speed,
-                            "heading"   to bearing
-                        ))
-                    }
-                }
+                eventSink = events
+                localBroadcast?.registerReceiver(
+                    locationReceiver,
+                    IntentFilter(LocationForegroundService.ACTION_LOCATION)
+                )
             }
             override fun onCancel(arguments: Any?) {
-                LocationForegroundService.onLocationUpdate = null
+                localBroadcast?.unregisterReceiver(locationReceiver)
+                eventSink = null
             }
         })
     }
